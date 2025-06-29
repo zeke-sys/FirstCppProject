794e190f4eeeee484c1a49386edafb0431ca4c6f
zeke-sys <zek3.isaac@gmail.com>
2025-06-28 21:07:56 -0600

    first commit

---
+++ hello.cpp
@@ -0,0 +1,14 @@
+#include <iostream> // This header file is required for input and output operations in C++.
+/*int main() {
+    std::cout << "Hello, World!" << std::endl; //print to console
+    std::cout << "This is a C++17 program." << std::endl;
+    return 0; //returning zero to indicate successful execution
+}*/
+
+using namespace std; //tells the comipiler to use the standard namespace
+int main() {
+    cout << "Hello, World!" << endl; //print to console
+    cout << "This is a C++17 program." << endl;
+    cout << "The answer is " << 6 * 7 << "." << endl; //calculating and printing the answer
+    return 0; //returning zero to indicate successful execution
+}
